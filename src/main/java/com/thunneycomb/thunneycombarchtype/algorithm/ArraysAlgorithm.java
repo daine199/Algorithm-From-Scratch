ad50f3a7ea0978f4f3bc545589448b574dfbda1f
黄一峰 <yifeng.huang@dianping.com>
2021-11-23 12:58:23 +0800

    java排序练习。

--- src/main/java/com/thunneycomb/thunneycombarchtype/algorithm/ArraysAlgorithm.java
+++ src/main/java/com/thunneycomb/thunneycombarchtype/algorithm/ArraysAlgorithm.java
@@ -7,6 +7,21 @@ import java.util.List;
 public class ArraysAlgorithm {
 
     public static int removeDuplicates(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        int index;
+        int left = 0;
+        for (int right = 1; right < nums.length; right++) {
+            if (nums[left] != nums[right]) {
+                left++;
+                nums[left] = nums[right];
+            }
+        }
+        index = left + 1;
+        System.out.println(String.format("Strip后长度为: %s", index));
+        return index;
+    }
+
+    public static int removeDuplicates_0(int[] nums) {
         /**
          * 去重数组
          */
@@ -18,12 +33,11 @@ public class ArraysAlgorithm {
         int left = 0;
         int index = 1;
 
-        for(int right = 1 ; right < nums.length; right++) {
+        for (int right = 1; right < nums.length; right++) {
             if (nums[left] == nums[right]) {
                 System.out.println();
                 continue;
-            }
-            else {
+            } else {
                 left++;
                 index++;
                 nums[left] = nums[right];
@@ -41,7 +55,7 @@ public class ArraysAlgorithm {
             return 0;
         }
         int left = 0;
-        for (int right = 1; right < nums.length; right++ ) {
+        for (int right = 1; right < nums.length; right++) {
             if (nums[left] != nums[right]) {
                 left++;
                 nums[left] = nums[right];
@@ -58,27 +72,21 @@ public class ArraysAlgorithm {
     }
 
     public static int removeDuplicates_3(int[] nums) {
-        if (nums == null || nums.length == 0)
-            return 0;
-
-        List list = new ArrayList();
-        for (int i = 0; i < nums.length; i++) {
-            list.add(nums[i]);
-        }
-        System.out.println(list);
-    return 0;
-    }
-
-    public static int removeDuplicates_4(int[] nums) {
+        /**
+         * 能够完成排序后输出
+         */
         if (nums == null || nums.length == 0) return 0;
-        int[] arr = quickSort(nums, 0, nums.length-1);
+        int[] arr = quickSort(nums, 0, nums.length - 1);
         return removeDuplicates_2(arr);
     }
 
     public static int[] quickSort(int[] arr, int low, int high) {
+        /**
+         * 快速排序
+         */
         int p, i, j, temp;
 
-        if (low >= high) return new int[] {};
+        if (low >= high) return new int[]{};
 
         p = arr[low];
         i = low;
@@ -94,8 +102,66 @@ public class ArraysAlgorithm {
         arr[low] = arr[i];
         arr[i] = p;
 
-        quickSort(arr, low, j-1);
-        quickSort(arr, j+1, high);
+        quickSort(arr, low, j - 1);
+        quickSort(arr, j + 1, high);
 
-        return arr;    }
+        return arr;
+    }
+
+    public static int[] bubSort_0(int[] arr) {
+        /**
+         * 原始冒泡
+         */
+        long start_time = System.nanoTime();
+        long start_time_ms = System.currentTimeMillis();
+        int[] arr_done = new int[]{};
+        int swap = 0;
+        for(int n = 0; n < arr.length; n++) {
+            for (int i = 0; i < arr.length -1; i++)
+            if (arr[i] > arr[i+1]) {
+                swap = arr[i]; arr[i] = arr[i+1]; arr[i+1] = swap;
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+        System.out.println(arr[i]);
+        }
+        long end_time = System.nanoTime();
+        long end_time_ms = System.currentTimeMillis();
+        System.out.println(String.format("bugSort_0运行时长%s ns (%s ms)",(end_time - start_time), (end_time_ms - start_time_ms)));
+        return arr_done;
+    }
+
+    public static int[] bubSort_1(int[] arr) {
+        /**
+         * 优化1 如果出现已排好的情况，则直接跳过计算
+         */
+        long start_time = System.nanoTime();
+        long start_time_ms = System.currentTimeMillis();
+        int[] arr_done = new int[]{};
+
+        int swap = 0;
+        boolean flag = true; // 默认是需要交换来完成排序
+        for(int n = 0; n < arr.length; n++) {
+            if (flag) { // 判断需要交换标志位， 一旦内部排序逻辑发现此次已经完全不需要交换时，提前结束排序
+                flag = false; // 设定下次不需要执行
+                for (int i = 0; i < arr.length -1; i++) {
+                    if (arr[i] > arr[i+1]) {
+                        flag = true; // 循环中一旦发生过交换，则设置下次需要继续交换
+                        swap = arr[i]; arr[i] = arr[i+1]; arr[i+1] = swap;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+
+        long end_time = System.nanoTime();
+        long end_time_ms = System.currentTimeMillis();
+        long time = end_time - start_time;
+        long time_ms = end_time_ms - start_time_ms;
+        System.out.println(String.format("bugSort_1运行时长%s ns (%s ms)", time, time_ms));
+        return arr_done;
+    }
 }
--- src/test/java/com/thunneycomb/thunneycombarchtype/algorithm/ArraysAlgorithmTest.java
+++ src/test/java/com/thunneycomb/thunneycombarchtype/algorithm/ArraysAlgorithmTest.java
@@ -3,22 +3,19 @@ package com.thunneycomb.thunneycombarchtype.algorithm;
 import com.thunneycomb.thunneycombarchtype.TestNGCaseBase;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.testng.annotations.Test;
-
-import com.thunneycomb.thunneycombarchtype.algorithm.ArraysAlgorithm;
 
 public class ArraysAlgorithmTest extends TestNGCaseBase {
 
     private final static Logger LOGGER = LoggerFactory.getLogger(ArraysAlgorithmTest.class);
 
     int[] nums = new int[] {1, 1, 2, 2, 3};
-    int[] nums2 = new int[] {1, 3, 5, 34, 23, 1, 2, 2, 3};
+    int[] nums2 = new int[] {1, 3, 5, 34, 23, 1, 2, 2, 3, 24, 311, 356 ,231, 567, 11, 1, 54, 2, 3, 66};
 
     @Test
     public void removeDuplicatesTest() {
 
-        int strip_len =  ArraysAlgorithm.removeDuplicates(nums);
+        int strip_len =  ArraysAlgorithm.removeDuplicates_0(nums);
 
         LOGGER.info("函数原始长度为{}, 新长度为{}",nums.length, strip_len);
     };
@@ -37,7 +34,7 @@ public class ArraysAlgorithmTest extends TestNGCaseBase {
 
     @Test
     public void removeDuplicates_4Test() {
-        int strip_len = ArraysAlgorithm.removeDuplicates_4(nums2);
+        int strip_len = ArraysAlgorithm.removeDuplicates_3(nums2);
         LOGGER.info("函数原始长度为{}, 新长度为{}",nums2.length, strip_len);
     }
 
@@ -46,5 +43,15 @@ public class ArraysAlgorithmTest extends TestNGCaseBase {
         int[] arr = ArraysAlgorithm.quickSort(nums2, 0, nums2.length - 1);
         int strip_len = ArraysAlgorithm.removeDuplicates_3(arr);
         LOGGER.info("函数原始长度为{}, 新长度为{}",nums.length, strip_len);
+    }
+
+    @Test
+    public void bubSort_0Test() {
+        int[] arr = ArraysAlgorithm.bubSort_0(nums2);
+    }
+
+    @Test
+    public void bubSort_1Test() {
+        int[] arr = ArraysAlgorithm.bubSort_1(nums2);
     }
 }
